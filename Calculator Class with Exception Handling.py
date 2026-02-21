7269344befd7aee3cd046a55f5ac3a64e46ca3a6
Shrutika Barmase <barmaseshrutika@gmail.com>
2026-02-21 21:28:03 +0530

    Add files via upload

---
+++ Calculator Class with Exception Handling.py
@@ -0,0 +1,18 @@
+class Calculator:
+
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        try:
+            return a / b
+        except ZeroDivisionError:
+            return "Error! Division by zero is not allowed."
+
+
